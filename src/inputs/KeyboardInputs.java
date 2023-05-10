d1673c7137594741763390582a6e81560b466df9
Phạm Quỳnh Trang <76781716+chanitypham@users.noreply.github.com>
2023-05-10 14:22:14 +0700

    Add files via upload

---
+++ src/inputs/KeyboardInputs.java
@@ -0,0 +1,10 @@
+package inputs;
+
+public class KeyboardInputs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/inputs/MouseInputs.java
@@ -0,0 +1,10 @@
+package inputs;
+
+public class MouseInputs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
