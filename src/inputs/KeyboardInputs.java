a69ebb8d2aa5c5f06d49bafd33c8ecd657c8af34
Phạm Quỳnh Trang <76781716+nhimchienxuuu@users.noreply.github.com>
2023-06-05 11:59:51 +0700

    update

---
+++ src/inputs/KeyboardInputs.java
@@ -0,0 +1,42 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import gamestates.Gamestate;
+import main.GamePanel;
+
+public class KeyboardInputs implements KeyListener {
+
+	private GamePanel panel;
+
+	public KeyboardInputs(GamePanel panel) {
+		this.panel = panel;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (Gamestate.state) {
+		case PLAYING:
+			panel.getGame().getPlaying().keyReleased(e);
+			break;
+		default:
+			break;
+
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (Gamestate.state) {
+		case PLAYING:
+			panel.getGame().getPlaying().keyPressed(e);
+			break;
+		}
+	}
+}
